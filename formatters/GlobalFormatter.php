1886df3b7244ca72891e6d89b6459378fd90d413
Mateus Torquato de Medeiros <matteus.torquato@gmail.com>
2019-08-18 22:23:35 -0300

    Adicionando meus formatters

---
+++ formatters/GlobalFormatter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace app\formatters;
+
+use yii\i18n\Formatter;
+
+class GlobalFormatter extends Formatter
+{
+    public static function asCpf($cpf)
+    {
+        return substr($cpf, 0, 3) . '.' . substr($cpf, 3, 3) . '.' . substr($cpf, 7, 3) . '-' . substr($cpf, -2);
+    }
+
+    public static function asTelefone($telefone)
+    {
+        if (strlen($telefone) == 11) {
+            return '(' . substr($telefone, 0, 2) . ') ' . substr($telefone, 2, 5) . '-' . substr($telefone, 7, 4);
+        } else {
+            return '(' . substr($telefone, 0, 2) . ') ' . substr($telefone, 2, 4) . '-' . substr($telefone, 6, 4);
+        }
+    }
+}
